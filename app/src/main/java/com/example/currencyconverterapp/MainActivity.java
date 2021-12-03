0d89e6b47400692e3caf765657a0bb80300a21a5
arthurmdros <arthurmedeiros2005@gmail.com>
2021-12-03 11:08:33 -0300

    Nota: os valores de entrada de elementos presentes na activity só podem ser recuperados no método onCreate

--- app/src/main/java/com/example/currencyconverterapp/MainActivity.java
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.currencyconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText value = findViewById(R.id.editValue);
+
+        value.getText();
     }
 }
