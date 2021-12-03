b1952fd72195072a87e5a461bec17b8fcd04d5ae
arthurmdros <arthurmedeiros2005@gmail.com>
2021-12-03 11:31:03 -0300

    Exibindo as três formas (implementando interface, instânciando uma variável, criando um método) possíveis de tratamento com a interação do botão

--- app/src/main/java/com/example/currencyconverterapp/MainActivity.java
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.currencyconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -20,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        // Primeira forma
+        this.mViewHolder.buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    // Segunda forma
+    public void teste(View view) {
+    }
+
+    // Terceira forma
+    @Override
+    public void onClick(View v) {
 
     }
 
